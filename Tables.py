ce20365b18b53475ae029dcb9a7e4da33fef4f77
Alcoholicorn <alcoholicorn@gmail.com>
2018-04-01 15:20:32 -0400

    added all the method calls for the tables that we currently have

--- Tables.py
+++ Tables.py
@@ -21,6 +21,66 @@ class Tables:
         self.bot = bot
 
     @commands.command(pass_context=True)
+    async def armors(self, ctx):
+        from MasterEquipment.Implements import ImplementArmor
+        potato = ImplementArmor.armorList
+        await self.printList(potato)
+
+    @commands.command(pass_context=True)
+    async def drawnVehicles(self, ctx):
+        from MasterEquipment.Implements import ImplementDrawnVehicles
+        potato = ImplementDrawnVehicles.drawnVehiclesList
+        await self.printList(potato)
+
+    @commands.command(pass_context=True)
+    async def expenses(self, ctx):
+        from MasterEquipment.Implements import ImplementExpenses
+        potato = ImplementExpenses.LifeExpenses
+        await self.printList(potato)
+
+    @commands.command(pass_context=True)
+    async def FDLodging(self, ctx):
+        from MasterEquipment.Implements import ImplementFDLodging
+        potato = ImplementFDLodging.lodgingList
+        await self.printList(potato)
+
+    @commands.command(pass_context=True)
+    async def mounts(self, ctx):
+        from MasterEquipment.Implements import ImplementMounts
+        potato = ImplementMounts.mountsList
+        await self.printList(potato)
+
+    @commands.command(pass_context=True)
+    async def adventuringGear(self, ctx):
+        from MasterEquipment.Implements import ImplementsAdventuringGear
+        potato = ImplementsAdventuringGear.adventuringGear
+        await self.printList(potato)
+
+    @commands.command(pass_context=True)
+    async def services(self, ctx):
+        from MasterEquipment.Implements import ImplementServices
+        potato = ImplementServices.servicesList
+        await self.printList(potato)
+
+    @commands.command(pass_context=True)
+    async def tools(self, ctx):
+        from MasterEquipment.Implements import ImplementTools
+        potato = ImplementTools.toolsList
+        await self.printList(potato)
+
+    @commands.command(pass_context=True)
+    async def tradeGoods(self, ctx):
+        from MasterEquipment.Implements import ImplementTradeGoods
+        potato = ImplementTradeGoods.tradeGoodList
+        await self.printList(potato)
+
+    @commands.command(pass_context=True)
+    async def trinkets(self, ctx):
+        from MasterEquipment.Implements import ImplementTrinkets
+        potato = ImplementTrinkets.trinketsList
+        await self.printList(potato)
+
+    @commands.command(pass_context=True)
     async def waterborne(self, ctx):
         from MasterEquipment.Implements import ImplementWaterborne
         potato = ImplementWaterborne.waterborneList
